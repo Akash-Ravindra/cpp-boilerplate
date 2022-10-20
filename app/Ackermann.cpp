8ce33d88442ef2d724970e8c99e772bd20ed4517
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2022-10-19 21:09:28 -0400

    1.09 - 1.14 , 1.17 : Created, defined and updated the source file for Ackermann class

--- app/Ackermann.cpp
+++ app/Ackermann.cpp
@@ -1,10 +1,33 @@
 /**
  * @file ackermann.cpp
  * @author Akash Ravindra (aravind2@umd.edu)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-10-18
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
+
+#include "../include/Ackermann.hpp"
+
+#include <array>
+
+steering::Ackermann::Ackermann(double track_w, double w_base,
+                               std::shared_ptr<pd::PidController> lwheel,
+                               std::shared_ptr<pd::PidController> rwheel)
+    : wheelBase(w_base),
+      trackWidth(track_w),
+      leftWheelController(lwheel),
+      rightWheelController(rwheel) {}
+steering::Ackermann::~Ackermann() {}
+
+std::array<double, 2> steering::Ackermann::computeVelocity(
+    double robotVelocity) {
+  return std::array<double, 2>{0.0, 0.0};
+}
+
+std::array<double, 2> steering::Ackermann::computeSteerAngle(
+    double currHead, double targetHead) {
+  return std::array<double, 2>{0.0, 0.0};
+}
