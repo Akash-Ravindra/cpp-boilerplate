ba3f83e4504df30ac19e8fcd032e28a548b0cfe8
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2022-10-19 21:11:59 -0400

    1.24: Fixing build error due to out dated main

--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,12 @@
 /**
  * @file main.cpp
  * @author Akash Ravindra (aravind2@umd.edu)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-10-18
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
- int main(){
-    return 0;
- }
+int main() { return 0; }
