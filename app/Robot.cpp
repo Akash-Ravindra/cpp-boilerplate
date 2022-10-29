fcf6631820349994080c703dbc11b59f78b8b51b
lilnpuma <manump@terpmail.umd.edu>
2022-10-28 22:47:18 -0400

    2.12,2.13 : Completed the implementation of Robot class and modified code struct

--- app/Robot.cpp
+++ app/Robot.cpp
@@ -11,6 +11,7 @@
 #include "../include/Robot.hpp"
 
 #include <array>
+#include <cmath>
 #include <memory>
 #include <vector>
 
@@ -23,19 +24,45 @@ bot::Robot::Robot(double maxAngle, double maxVel, double wBase, double tWidth)
       wheelBase{wBase},
       trackWidth{tWidth} {
   std::shared_ptr<pd::PidController> lWheel(
-      std::make_shared<pd::PidController>(0.0, 0.0, 0.01));
+      std::make_shared<pd::PidController>(3.5, 0.0, 0.01));
   std::shared_ptr<pd::PidController> rWheel(
-      std::make_shared<pd::PidController>(0.0, 0.0, 0.01));
-  steering = std::make_shared<steering::Ackermann>(trackWidth, wheelBase,
-                                                   lWheel, rWheel);
-  velocityController = std::make_shared<pd::PidController>(0.0, 0.0, 0.01);
+      std::make_shared<pd::PidController>(3.5, 0.0, 0.01));
+  std::shared_ptr<pd::PidController> velocityContr(
+      std::make_shared<pd::PidController>(14, 0.001, 0.01));
+
+  steering = std::make_shared<steering::Ackermann>(
+      trackWidth, wheelBase, lWheel, rWheel, velocityContr);
 }
 bot::Robot::~Robot() {}
 
 std::vector<std::array<double, 2>> bot::Robot::moveRobot(double targetVel,
                                                          double targetHead) {
-  steering->computeSteerAngle(headingCurrent, targetHead);
-  double tarVel = velocityController->compute(targetVel, velocityCurrent);
-  steering->computeVelocity(tarVel);
-  return std::vector<std::array<double, 2>>{{0.0, 0.0}, {0.0, 0.0}};
+  double velThreshold{0.001};
+  double angleThreshold{0.0872665};
+  int iterationThreshold{50};
+  int iterCount{0};
+  std::array<double, 2> opWheelAngle{0, 0};
+  std::array<double, 2> opWheelVelocity{0, 0};
+  std::vector<std::array<double, 2>> outputValues{
+      {velocityCurrent, headingCurrent}};
+
+  while ((fabs(targetVel - velocityCurrent) > velThreshold) &&
+         (iterCount < iterationThreshold)) {
+    // Calculate new wheel Angles
+    opWheelAngle = steering->computeSteerAngle(headingCurrent, targetHead);
+    // Calculate new wheel Velocity
+    opWheelVelocity = steering->computeVelocity(targetVel);
+    // Calculate new robot Velocity
+    velocityCurrent = opWheelVelocity[0] * wheelBase / sin(opWheelAngle[0]);
+    // Calculate new robot Heading
+    headingCurrent += ((velocityCurrent / wheelBase) *
+                       atan2(2 * tan(opWheelAngle[0]) * tan(opWheelAngle[1]),
+                             (opWheelAngle[0]) + tan(opWheelAngle[1]))) *
+                      0.01;
+    // Saving new velocity and heading in a vector
+    outputValues.push_back(std::array<double, 2>{
+        velocityCurrent, std::fmod(headingCurrent, 3.14159)});
+    iterCount++;
+  }
+  return outputValues;
 }
